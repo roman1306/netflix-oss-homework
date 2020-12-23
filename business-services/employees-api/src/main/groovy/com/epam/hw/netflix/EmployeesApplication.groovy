aae2f701490888a24ec44c242c3f9d6c53b1e4b0
Roman Petrenko <rpetrenko51@gmail.com>
2020-12-23 21:33:27 +0400

    completed settings

--- business-services/employees-api/src/main/groovy/com/epam/hw/netflix/EmployeesApplication.groovy
+++ business-services/employees-api/src/main/groovy/com/epam/hw/netflix/EmployeesApplication.groovy
@@ -2,8 +2,12 @@ package com.epam.hw.netflix
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
 class EmployeesApplication {
     static void main(String[] args) {
         SpringApplication.run(EmployeesApplication, args)
--- business-services/employees-api/src/main/groovy/com/epam/hw/netflix/controllers/EmployeeAPIController.groovy
+++ business-services/employees-api/src/main/groovy/com/epam/hw/netflix/controllers/EmployeeAPIController.groovy
@@ -3,6 +3,7 @@ package com.epam.hw.netflix.controllers
 import com.epam.hw.netflix.api.WorkspaceAPI
 import com.epam.hw.netflix.services.EmployeeService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -26,7 +27,7 @@ class EmployeeAPIController {
                 firstName: employee.firstName,
                 lastName : employee.lastName,
                 email    : employee.email,
-                workspace: null // null? Nope. Let's request exact workspace by employee.workspaceId from workspaces-api. How? With feign client maybe?
+                workspace: workspaceAPIClient.getWorkspaceById(id) // null? Nope. Let's request exact workspace by employee.workspaceId from workspaces-api. How? With feign client maybe?
         ]
     }
 }
